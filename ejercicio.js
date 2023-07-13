d6ce6f6512b9d6edaa9d9811680445baf848c7aa
Brahian-MontoyaA <124197730+Brahian-MontoyaA@users.noreply.github.com>
2023-07-12 19:26:05 -0500

    ff fff

--- ejercicio.js
+++ ejercicio.js
@@ -3,7 +3,8 @@ var papel1 = lienzo1.getContext("2d");
 var lienzo2 = document.getElementById("canvas2");
 var papel2 = lienzo2.getContext("2d");
 var imagen1 = new Image();
-imagen1.src = "";
+imagen1.src =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Elpenolantioquia.JPG/275px-Elpenolantioquia.JPG=";
 imagen1.addEventListener("load", dibujar);
 function dibujar() {
   papel1.drawImage(imagen1, 200, 200, 100, 100);
