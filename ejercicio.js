67526eba2b1da6e71893733d3bbdca35cc9a3066
Brahian-MontoyaA <124197730+Brahian-MontoyaA@users.noreply.github.com>
2023-07-12 19:21:01 -0500

    ee ee

--- ejercicio.js
+++ ejercicio.js
@@ -3,8 +3,7 @@ var papel1 = lienzo1.getContext("2d");
 var lienzo2 = document.getElementById("canvas2");
 var papel2 = lienzo2.getContext("2d");
 var imagen1 = new Image();
-imagen1.src =
-  "2wCEAAoHCBUVFRgVFhYZGBgaGhwYGRocGBgYHBgcGhwaHBwcGhocIS4lHB8rHxkYJjgmLC8xNTU1GiQ7QDs0Py40NTEBDAwMEA8QGBISGjEhISExNDQ0NDQ0MTE0MTQ0NDQ0NDQ0NDQ0NDQxNDQ0NDQ=";
+imagen1.src = "";
 imagen1.addEventListener("load", dibujar);
 function dibujar() {
   papel1.drawImage(imagen1, 200, 200, 100, 100);
